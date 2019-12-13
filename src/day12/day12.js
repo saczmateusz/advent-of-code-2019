2c1a8554c724180d228d7d6fa65512b5f88b6cde
Mateusz Saczawa <sacz.mateusz@gmail.com>
2019-12-13 01:24:36 +0100

    Day 12

--- src/day12/day12.js
+++ src/day12/day12.js
@@ -12,7 +12,7 @@ function runScript() {
             return moon;
         });
 
-    const data = Array.from(dataOriginal);
+    let data = JSON.parse(JSON.stringify(dataOriginal));
 
     function calculateGravityImpact() {
         for (let i = 0; i < data.length; i += 1) {
@@ -22,11 +22,15 @@ function runScript() {
                         data[i][k + 3] =
                             data[i][k] < data[j][k]
                                 ? data[i][k + 3] + 1
-                                : (data[i][k] !== data[j][k] ? data[i][k + 3] - 1 : data[i][k + 3]);
+                                : data[i][k] !== data[j][k]
+                                ? data[i][k + 3] - 1
+                                : data[i][k + 3];
                         data[j][k + 3] =
                             data[i][k] < data[j][k]
                                 ? data[j][k + 3] - 1
-                                : (data[i][k] !== data[j][k] ? data[j][k + 3] + 1 : data[j][k + 3]);
+                                : data[i][k] !== data[j][k]
+                                ? data[j][k + 3] + 1
+                                : data[j][k + 3];
                     }
                     continue;
                 }
@@ -60,31 +64,72 @@ function runScript() {
         for (let i = 0; i < count; i += 1) {
             calculateGravityImpact();
             applyVelocity();
-            console.log(`ITERATION ${i}`);
         }
         return totalEnergy();
     }
 
-    const t0 = performance.now();
-    const result = simulateIterations(1000);
+    function findRepeat() {
+        const terms = [0, 0, 0];
+        for (let k = 0; k < 3; k += 1) {
+            data = JSON.parse(JSON.stringify(dataOriginal));
+            let iteration = 2;
+            while (1) {
+                calculateGravityImpact();
+                applyVelocity();
+                if (
+                    data[0][k] === dataOriginal[0][k] &&
+                    data[1][k] === dataOriginal[1][k] &&
+                    data[2][k] === dataOriginal[2][k] &&
+                    data[3][k] === dataOriginal[3][k]
+                ) {
+                    if (terms[k] === 0) {
+                        terms[k] = iteration;
+                    }
+                }
+                if (terms[k] !== 0) break;
+                iteration += 1;
+            }
+        }
+        return terms;
+    }
+
+    function gcd(a, b) {
+        let t = 0;
+        if (a < b) {
+            t = b;
+            b = a;
+            a = t;
+        }
+        t = a % b;
+        return t ? gcd(b, t) : b;
+    }
+
+    function lcm(a, b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    let t0 = performance.now();
+    const result1 = simulateIterations(1000);
     const t1 = performance.now() - t0;
 
-    // t0 = performance.now();
-
-    // t1 = performance.now() - t0;
+    t0 = performance.now();
+    const terms = findRepeat();
+    const temp = lcm(terms[0], terms[1]);
+    const result2 = lcm(temp, terms[2]);
+    const t2 = performance.now() - t0;
     console.log('===========================');
     console.log('==');
     console.log('==  Day 12');
     console.log('==');
     console.log('==  Part 1:');
-    console.log(`==  ${result}`);
+    console.log(`==  ${result1}`);
     console.log('==');
     console.log(`==  Execution time: ${t1.toFixed(3)} ms`);
-    // console.log('==');
-    // console.log('==  Part 2:\n');
-    // console.log(`==  ${result}`);
-    // console.log('==');
-    // console.log(`==  Execution time: ${t1.toFixed(3)} ms`);
+    console.log('==');
+    console.log('==  Part 2:\n');
+    console.log(`==  ${result2}`);
+    console.log('==');
+    console.log(`==  Execution time: ${t2.toFixed(3)} ms`);
     console.log('==');
     console.log('===========================\n');
 }
